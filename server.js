399ce88f297fd8599e772e827f130e16e0ed5db5
Eduardo Henrique <eduwphone@hotmail.com>
2021-09-14 20:14:11 -0300

    conexão
    atualizando patch

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-//test
+require("dotenv").config();
+const express = require("express");
+const connectToDb = require("./src/database/database");
 
-// teste 2
+connectToDb();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => console.info(`Servidor rodando em http://localhost:${port}`)
+);
